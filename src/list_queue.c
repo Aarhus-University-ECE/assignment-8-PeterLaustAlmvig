28d2f02864be192bbac37591854c48b54b6a4511
Peter Laust Almvig <petealmvig@gmail.com>
2022-10-26 23:43:03 +0200

    First commit

--- src/list_queue.c
+++ src/list_queue.c
@@ -4,19 +4,47 @@
 void init_queue(queue *q)
 {
   // Add your init_queue
+  q->size = 0;
+  q->front = NULL;
+  q->rear = NULL;
 }
 
 int empty(queue *q)
 {
-  // Add your empty function  
+  // Add your empty function
+  return q->size == 0;
 }
 
 void enqueue(queue *q, int x)
 {
   // Add your enqueue function
+
+  //add node to end
+  qnode *element = malloc(sizeof(qnode));
+  element->next = q->rear;
+  element->data = x;
+  q->rear = element;
+  
+  q->front = q->rear;
+  for(int i = 0; i<q->size; i++){
+    q->front = q->front->next;
+  }
+  q->size++;
 }
 
 int dequeue(queue *q)
 {
   // Add your dequeue function
+  assert(q->size != 0);
+  int x = q->front->data;
+
+  q->front = q->rear;
+  for(int i = 0; i<q->size; i++){
+    //rintf("before: q.front.data = %d\t", q->front->data);
+    if(q->front->next != NULL){
+      q->front = q->front->next;
+    }
+  }
+  q->size--;
+  return x;
 }
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,34 @@
 extern "C"{
     // Add the header files required to run your main 
+    #include "list_queue.h"
+    #include "stdio.h"
 }
 
 // File for sandboxing and trying out code
 int main(int argc, char **argv)
 {
+    queue q;
+    init_queue(&q);
+    printf("ENQUEUE: \n\n");
+    //printf("front: %d\n", q.front->data);
+    enqueue(&q, 5);
+    printf("front: %d\n", q.front->data);
+    enqueue(&q, 10);
+    printf("front: %d\n", q.front->data);
+    enqueue(&q, 15);
+    printf("front: %d\n", q.front->data);
+    enqueue(&q, 20);
+    printf("front: %d\n", q.front->data);
+
+    printf("DEQUEUE:\n\n");
+
+    dequeue(&q);
+    printf("front: %d\n", q.front->data);
+    dequeue(&q);
+    printf("front: %d\n", q.front->data);
+    dequeue(&q);
+    printf("front: %d\n", q.front->data);
+    dequeue(&q);
+    printf("front: %d\n", q.front->data);
     return 0;
 }

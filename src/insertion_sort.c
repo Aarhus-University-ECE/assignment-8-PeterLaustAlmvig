168c9eae5d37b7956af4d88dc2ffa82e444d1d99
Peter Laust Almvig <petealmvig@gmail.com>
2022-10-31 22:50:43 +0100

    insertion_sort virker(?)

--- src/insertion_sort.c
+++ src/insertion_sort.c
@@ -6,4 +6,12 @@
 void sort(linked_list *llPtr)
 {
  // Add your sort function here
+ node_t* curr = llPtr->head->next;
+ node_t* prev = llPtr->head;
+
+ while(curr != NULL){
+    if(curr->data < prev->data){
+        
+    }
+ }
 }
